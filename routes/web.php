2622f4b4ad8d73a29c4d7437ebd7d9954d9fc320
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2022-02-13 12:01:21 +0530

    created controllers, models, migrations and fixed max key length issue

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodosController::class, 'index']);
+Route::resource('todo', 'App\Http\Controllers\TodosController');
